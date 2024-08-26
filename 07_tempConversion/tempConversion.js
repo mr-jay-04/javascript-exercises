53898e38b6e78ca20b4e6c6010394381a617757f
mr-jay <171690269+mr-jay-04@users.noreply.github.com>
2024-08-27 00:28:01 +0530

    Conversion of temp

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempr) {
+  const tempInCelsius = (tempr-32)*(5/9);
+  return +tempInCelsius.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempr) {
+  const tempInFarenheit = tempr * 9/5 + 32;
+  return +tempInFarenheit.toFixed(1);
 };
 
 // Do not edit below this line
