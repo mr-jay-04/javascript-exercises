a7f8ca226e2e1d7e03f2f6eb947bae488da3963a
mr-jay <171690269+mr-jay-04@users.noreply.github.com>
2024-08-26 22:58:08 +0530

    Add number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let sum = 0;
 
+    // if (Number.isNaN(num1) || Number.isNaN(num2)) return "ERROR";
+
+    if (num1<0 || num2<0) return "ERROR";
+
+    if (!(Number.isInteger(num1) && Number.isInteger(num2))) return "ERROR";
+
+
+
+    let lowerLimit= num1>=num2? num2 : num1;
+    let upperLimit= num1>=num2? num1 : num2;
+    for (i=lowerLimit; i<=upperLimit; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
