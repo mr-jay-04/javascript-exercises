2ab964765b08a536e860bf7f47010a7d3e785d5a
mr-jay <171690269+mr-jay-04@users.noreply.github.com>
2024-08-27 22:31:54 +0530

    Add factorial function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return (num1 + num2) ;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return (num1-num2);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	for (let num of array){
+    sum += num;
+  }
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let product = 1;
+  for (let num of array)
+    product *= num;
 
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base,exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1) return 1;
+  else 
+    return num * factorial(num-1);
 };
 
 // Do not edit below this line
